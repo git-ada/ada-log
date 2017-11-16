b485c4c9e70311ee0d1bc28912392b7cdad4c8be
咸菜 <ASUS@DESKTOP-UTSB1A5>
2017-11-16 16:35:32 +0800

    ssss

--- src/main/java/com/ada/log/service/impl/LogServiceImpl.java
+++ src/main/java/com/ada/log/service/impl/LogServiceImpl.java
@@ -1,6 +1,12 @@
 package com.ada.log.service.impl;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import com.ada.log.service.LogService;
 
@@ -9,7 +15,17 @@ import com.ada.log.service.LogService;
  *
  */
 @Service
+
 public class LogServiceImpl implements LogService{
+	@Autowired
+    private  JedisPool jedisPool;//非切片连接池
+    
+	public JedisPool getJedisPool() {
+		return jedisPool;
+	}
+	public void setJedisPool(JedisPool jedisPool) {
+		this.jedisPool = jedisPool;
+	}
 
 	/**
 	 * 记录日志
@@ -22,6 +38,9 @@ public class LogServiceImpl implements LogService{
 	 * @param browsingPage  当前页面链接
 	 */
 	public void log(String ipAddress,String uuid, Integer siteId, Integer channelId,String clickNum, String browsingTime, String browsingPage) {
+		
+		
+		
 	}
 
 	/**
@@ -30,7 +49,8 @@ public class LogServiceImpl implements LogService{
 	 * @param ipAddress    IP地址
 	 */
 	protected void putSiteIPSet(Integer siteId,String ipAddress) {
-		
+		Jedis jedis = jedisPool.getResource();
+		jedis.sadd("SiteIP_"+siteId+"", ipAddress);
 	}
 	
 	/**
@@ -39,7 +59,8 @@ public class LogServiceImpl implements LogService{
 	 * @param ipAddress
 	 */
 	protected void increSitePV(Integer siteId) {
-		
+		Jedis jedis = jedisPool.getResource();
+		jedis.incr("SitePV_"+siteId+"");
 	}
 	
 	/**
@@ -48,18 +69,20 @@ public class LogServiceImpl implements LogService{
 	 * @param ipAddress    IP地址
 	 */
 	protected void putChannelIPSet(Integer channelId,String ipAddress){
-		;
+		Jedis jedis = jedisPool.getResource();
+		jedis.sadd("ChannelIP_"+channelId+"", ipAddress);
 	}
 	
 	/**
-	 * 保存站点PV +1
-	 * @param siteId       站点ID
+	 * 保存渠道PV +1
+	 * @param channelId       渠道ID
 	 * @param ipAddress    IP地址
 	 */
 	protected void increChannelPV(Integer channelId) {
-		;	
+		Jedis jedis = jedisPool.getResource();
+		jedis.incr("ChannelPV_"+channelId+"");	
 	}
-
+	
 	/**
 	 * 
 	 * @param ipAddress   IP地址
@@ -67,7 +90,10 @@ public class LogServiceImpl implements LogService{
 	 * @return
 	 */
 	protected Integer increIPClickNum(String ipAddress,Integer pageClickNum){
-		return null;
+		Jedis jedis = jedisPool.getResource();
+		Long pageClickTotal = jedis.incrBy("CIPNum_"+ipAddress, pageClickNum);
+		int pageClickTotal2 = Integer.parseInt(String.valueOf(pageClickTotal)); 
+		return pageClickTotal2;
 	}
 	
 	/**
@@ -76,17 +102,8 @@ public class LogServiceImpl implements LogService{
 	 * @param ipClickNum  IP累计点击次数
 	 */
 	protected void updateChannelMultipleRangeClickIP(Integer channelId,Integer ipClickNum){
-		if(ipClickNum>=1 && ipClickNum<=2){
-			
-		}else if(ipClickNum>=1 && ipClickNum<=2){
-			
-		}else if(ipClickNum>=3 && ipClickNum<=5){
-			
-		}else if(ipClickNum>=6 && ipClickNum<=10){
-			
-		}else if(ipClickNum>10){
-			
-		}
+		Jedis jedis = jedisPool.getResource();
+		
 	}
 	
 	/**
@@ -96,16 +113,42 @@ public class LogServiceImpl implements LogService{
 	 * @return
 	 */
 	protected boolean matchTargetPage(Integer siteId,String browsingPage){
+		String[] split = browsingPage.split("\\?");
+		
+		if(split[0].contains("目标页面")){
+			return true;
+		}
+		
+		
 		return false;
 	}
-
+	/*测试*/
+	public static void main(String[] args) {
+		JedisPoolConfig config = new JedisPoolConfig();
+		
+		JedisPool jedisPool = new JedisPool(config, "127.0.0.1", 6379, 20000, "g^h*123T", 2);
+		LogServiceImpl logServiceImpl = new LogServiceImpl();
+		logServiceImpl.setJedisPool(jedisPool);
+		
+		String s= "http://sss.com?t=9";
+		String[] split = s.split("\\?");
+		String string = split[0];
+		System.out.println(string);
+		
+	}
+	
+	
 	/**
 	 * 
-	 * 渠道进入进入目标页IP集合
+	 * 渠道进入目标页IP集合
 	 * @param channelId
 	 */
 	protected void putChannelTIPSet(Integer channelId,String ipAddress){
-		
+		Jedis jedis = jedisPool.getResource();
+		jedis.sadd("ChannelTIP_"+channelId, ipAddress);
 	}
+	
+	
+	
 
 }
