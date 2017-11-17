4e1bf180f7935d14c88bef835286cddecdaf21e2
咸菜 <ASUS@DESKTOP-UTSB1A5>
2017-11-17 14:33:41 +0800

    更改：取出渠道IPSet集合、取出渠道进入目标页IPSet集合、取出站点IPSet集合

--- src/main/java/com/ada/log/service/impl/StatServiceImpl.java
+++ src/main/java/com/ada/log/service/impl/StatServiceImpl.java
@@ -22,21 +22,20 @@ public class StatServiceImpl implements StatService{
 
 	@Autowired
     private  JedisPools jedisPools;//自动切库,周一至周日每天一库
-    
-	
+
 	/*测试*/
 	public static void main(String[] args) throws Exception {
 		JedisPoolConfig config = new JedisPoolConfig();
 		
 		JedisPool jedisPool = new JedisPool(config, "127.0.0.1", 6379, 20000, "g^h*123T", 2);
 		StatServiceImpl statServiceImpl = new StatServiceImpl();
-		//statServiceImpl.setJedisPool(jedisPool);
+//		statServiceImpl.setJedisPool(jedisPool);
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		Date dt = df.parse("2017-11-15");
 		
-		//SiteStat statChannel = statServiceImpl.statSite(2, dt);
-		ChannelStat channelStat = statServiceImpl.statChannel(2, 2, dt);
-		System.out.println("channelPV:  ==> "+channelStat.getPv()+"channelIP:  ==> "+channelStat.getIp());
+		SiteStat statChannel = statServiceImpl.statSite(2, dt);
+//		ChannelStat channelStat = statServiceImpl.statChannel(2, 2, dt);
+		System.out.println("channelPV:  ==> "+statChannel.getPv()+"channelIP:  ==> "+statChannel.getIp());
 		
 	}
 
@@ -45,22 +44,18 @@ public class StatServiceImpl implements StatService{
 		//取出站点PV
 		Integer sitePV = Integer.valueOf(jedis.get("SitePV_"+site+""));
 		//取出站点IPSet集合
-		Set<String> siteIPSet = jedis.smembers("SiteIP_"+site+"");
-		int siteIP = siteIPSet.size();
-
+		int siteIP = jedis.scard("SiteIP_"+site+"").intValue();
 		return new SiteStat(site, siteIP, sitePV, date);
 	}
 	
 	public ChannelStat statChannel(Integer siteId, Integer channelId, Date date) {
 		Jedis jedis = getJedis(date);
 		//取出渠道IPSet集合
-		Set<String> channelIPSet = jedis.smembers("ChannelIP_"+channelId+"");
-		int channelIP = channelIPSet.size();
+		int channelIP = jedis.scard("ChannelIP_"+channelId+"").intValue();
 		//取出渠道PV 
 		Integer sitePV = Integer.valueOf(jedis.get("ChannelPV_"+channelId+""));
 		//取出渠道进入目标页IP集合
-		Set<String> targetpageipSet = jedis.smembers("ChannelTIP_"+channelId+"");
-		int targetpageIP = targetpageipSet.size();
+		int targetpageIP = jedis.scard("ChannelTIP_"+channelId+"").intValue();
 		//取出渠道多个点击区间次数
 		Integer clickip1 = Integer.valueOf(jedis.get("ChannelC1IP_"+channelId+""));
 		Integer clickip2 = Integer.valueOf(jedis.get("ChannelC2IP_"+channelId+""));
@@ -69,7 +64,7 @@ public class StatServiceImpl implements StatService{
 		return new ChannelStat(siteId, channelId, channelIP, sitePV, clickip1, clickip2, clickip3, clickip4, targetpageIP, date);
 	}
 
-	protected Jedis getJedis(Date date){
+	protected Jedis getJedis(Date date){	
 		return jedisPools.getResource(date.getDay());
 	}
 }
