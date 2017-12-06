aa677ee999cf369aa91dc62c4fe74e9a95bc6bf6
老腊肉 <ASUS@DESKTOP-OIJIP7L>
2017-12-06 10:01:22 +0800

    增加IP数据库

---
+++ src/main/java/com/ada/log/service/IPSetService.java
@@ -0,0 +1,22 @@
+package com.ada.log.service;
+
+/**
+ * 历史访问IP地址库，
+ */
+public interface IPSetService {
+
+	/**
+	 * 是否存在
+	 * @param ipAddress
+	 * @return
+	 */
+	public boolean exists(Integer domainId,String ipAddress);
+	
+	/**
+	 * 添加一个
+	 * @param domainId
+	 * @param ipAddress
+	 * @return
+	 */
+	public boolean add(Integer domainId,String ipAddress);
+}
---
+++ src/main/java/com/ada/log/service/impl/IPSetServiceImpl.java
@@ -0,0 +1,49 @@
+package com.ada.log.service.impl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import com.ada.log.service.IPSetService;
+
+@Service
+public class IPSetServiceImpl implements IPSetService {
+	
+	@Value("${redis.host:}")
+	private String host;
+	@Value("${redis.port:}")
+	private String port;
+	@Value("${redis.pass:}")
+	private String pass;
+	
+	@Autowired
+	private JedisPoolConfig jedisPoolConfig;
+	
+	private JedisPool ipSetJedisPool;  /** , **/
+	private Integer defualtDBindex =  8; /** 默认库 **/
+	
+	public void afterPropertiesSet() throws Exception {
+		JedisPoolConfig config = new JedisPoolConfig();
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig,host, Integer.valueOf(port), 1000, pass, defualtDBindex);
+	}
+
+	@Override
+	public boolean exists(Integer domainId, String ipAddress) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(Integer domainId, String ipAddress) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
