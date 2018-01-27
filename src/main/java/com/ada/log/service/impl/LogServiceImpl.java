0b788dc519d98fb1fc911557b2da61094338cf09
wanghl <wanghl@DESKTOP-R4F7L3U>
2018-01-27 13:08:57 +0800

    xxx

--- src/main/java/com/ada/log/service/impl/LogServiceImpl.java
+++ src/main/java/com/ada/log/service/impl/LogServiceImpl.java
@@ -77,7 +77,7 @@ public class LogServiceImpl implements LogService{
 						Integer maxBatch=  temp.size()/numberOfBatchSave + (temp.size()%numberOfBatchSave>0?1:0);
 						for(int i=0;i<maxBatch;i++){
 							List tlist = new ArrayList();
-							for(int j=i*numberOfBatchSave;j<numberOfBatchSave && j<temp.size();j++){
+							for(int j=i*numberOfBatchSave;j<temp.size();j++){
 								tlist.add(temp.get(j));
 								accessLogDao.batchInsert(tlist);
 							}
