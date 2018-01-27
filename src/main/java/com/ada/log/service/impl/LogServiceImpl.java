cc1443d3c0600f40a5dfe00fdd1f00947983e5fe
wanghl <wanghl@DESKTOP-R4F7L3U>
2018-01-27 14:51:48 +0800

    xxx

--- src/main/java/com/ada/log/service/impl/LogServiceImpl.java
+++ src/main/java/com/ada/log/service/impl/LogServiceImpl.java
@@ -53,7 +53,7 @@ public class LogServiceImpl implements LogService{
 	@Autowired
 	private AccessLogDao accessLogDao;
 	
-	private Integer numberOfBatchSave = 1000;
+	private Integer numberOfBatchSave = 10000;
 	
 	@Override
 	public void log(AccessLog data) {
