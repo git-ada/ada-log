116887040ea9eae446bba279553a0e59d5e76d94
wanghl <wanghl@DESKTOP-R4F7L3U>
2018-02-08 19:30:27 +0800

    xxx

--- src/main/java/com/ada/log/dao/impl/AccessLogDaoImpl.java
+++ src/main/java/com/ada/log/dao/impl/AccessLogDaoImpl.java
@@ -47,8 +47,11 @@ public class AccessLogDaoImpl implements AccessLogDao,InitializingBean {
 	protected void resetSql() {
 		SimpleDateFormat df =new SimpleDateFormat("yyyyMMdd");
 		String date = df.format(Dates.now());
-		insertAcccessLogSql = "INSERT INTO `ada_access_log_"+date+"`(siteId,domainId,channelId,adId,ipAddress,region,uuid,url,useragent,os,browser,screenSize,pageSize,referer,iframe,firstTime,todayTime,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
-		insertEventLogSql = "INSERT INTO `ada_event_log_"+date+"`(siteId,domainId,channelId,adId,ipAddress,region,uuid,url,event,args,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,now())";
+//		insertAcccessLogSql = "INSERT INTO `ada_access_log_"+date+"`(siteId,domainId,channelId,adId,ipAddress,region,uuid,url,useragent,os,browser,screenSize,pageSize,referer,iframe,firstTime,todayTime,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
+//		insertEventLogSql = "INSERT INTO `ada_event_log_"+date+"`(siteId,domainId,channelId,adId,ipAddress,region,uuid,url,event,args,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,now())";
+		insertAcccessLogSql = "INSERT INTO `ada_access_log` (siteId,domainId,channelId,adId,ipAddress,region,uuid,url,useragent,os,browser,screenSize,pageSize,referer,iframe,firstTime,todayTime,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,now())";
+		insertEventLogSql = "INSERT INTO `ada_event_log` (siteId,domainId,channelId,adId,ipAddress,region,uuid,url,event,args,requestTime,createTime) values (?,?,?,?,?,?,?,?,?,?,?,now())";
+
 	}
 	
 	public void createAccessTable(String data){
